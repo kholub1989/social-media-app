c7735a903a593925c7127ef5fb5904e5d5f436d5
Krisztian Holub <kholub1989@gmail.com>
2020-10-19 21:03:24 -0400

    adding  Authentication Context

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
+import { AuthContext } from "../context/auth";
 import { useForm } from "../util/hooks";
 
 function Login(props) {
+  const context = useContext(AuthContext);
   const [errors, setErrors] = useState({});
 
   const { onChange, onSubmit, values } = useForm(loginUserCallback, {
@@ -14,7 +16,8 @@ function Login(props) {
   });
 
   const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-    update(_, result) {
+    update(_, { data: { login: userData } }) {
+      context.login(userData);
       props.history.push("/");
     },
     onError(err) {
@@ -40,7 +43,6 @@ function Login(props) {
           error={errors.username ? true : false}
           onChange={onChange}
         />
-
         <Form.Input
           label="Password"
           placeholder="Password.."
@@ -50,7 +52,6 @@ function Login(props) {
           error={errors.password ? true : false}
           onChange={onChange}
         />
-
         <Button type="submit" primary>
           Login
         </Button>
